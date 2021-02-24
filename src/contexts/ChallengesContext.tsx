f1116bfbbf24ae2d104010ff6532c3df075bcfd1
Bernardo Marques <bernardohmarques@hotmail.com>
2021-02-24 19:21:36 -0300

    chore: add dinamic values to handle experience

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,19 +1,21 @@
 import { createContext, useState, ReactNode } from "react";
-import challenges from '../../challenges.json'
+import challenges from "../../challenges.json";
 
 interface Challenge {
-    type: 'body' | 'eye';
-    description: 'string';
-    amount: number
+  type: "body" | "eye";
+  description: "string";
+  amount: number;
 }
 
 interface ChallengesContextData {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
+  experienceToNextLevel: number;
   activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
+  resetChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -23,20 +25,26 @@ interface ChallengesProviderProps {
 export const ChallengesContext = createContext({} as ChallengesContextData);
 
 export const ChallengesProvider = ({ children }: ChallengesProviderProps) => {
-    const [level, setLevel] = useState(1);
-    const [currentExperience, setCurrentExperience] = useState(0);
-    const [challengesCompleted, setChallengesCompleted] = useState(0);
-    const [activeChallenge, setActiveChallenge] = useState(null)
+  const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
+  const [activeChallenge, setActiveChallenge] = useState(null);
+
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
   const levelUp = () => {
     setLevel(level + 1);
   };
 
   const startNewChallenge = () => {
-    const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
-    const challenge = challenges[randomChallengeIndex]
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
 
-    setActiveChallenge(challenge)
+    setActiveChallenge(challenge);
+  };
+
+  const resetChallenge = () => {
+    setActiveChallenge(null);
   };
 
   return (
@@ -47,7 +55,9 @@ export const ChallengesProvider = ({ children }: ChallengesProviderProps) => {
         challengesCompleted,
         levelUp,
         startNewChallenge,
-        activeChallenge
+        activeChallenge,
+        resetChallenge,
+        experienceToNextLevel
       }}
     >
       {children}
